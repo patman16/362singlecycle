3df5e994fefd575208d84d6c9d3cea191647030e
Cristian David Bedoya <cdb539@hush.ece.northwestern.edu>
2014-02-09 14:21:14 -0600

    updated ifu files

--- ifu.v
+++ ifu.v
@@ -53,30 +53,31 @@ endmodule
 module ifu(branch, zero, jump, clock, start, instruction);
 
     input branch, zero, jump, clock, start;	
-    output [31:0] instruction;
-    reg [31:0] four;
+    output [31:0] address, test, instruction;
+    reg [29:0] one;
     wire mux0control, open1, open2;
-    wire [31:0] adder0, adder1, mux0, mux1, jumpmux, pcout, extend, address, test; 
+    wire [29:0] adder0, adder1, mux0, mux1, jumpmux, pcout, extend; 
+   
     
-
-    adder_IFU #(.N(32)) adder0_map (pcout, four, 1'b0, adder0, open1);
-    adder_IFU #(.N(32)) adder1_map (adder0, extend, 1'b0, adder1, open2);
+    adder_IFU #(.N(30)) adder0_map (pcout, one, 1'b0, adder0, open1);
+    adder_IFU #(.N(30)) adder1_map (adder0, extend, 1'b0, adder1, open2);
     and (mux0control, branch, zero); 
-    mux_2to1_n #(.n(32)) mux0_map (adder0, adder1, mux0control, mux0);
-    mux_2to1_n #(.n(32)) mux1_map (mux0, jumpmux, jump, mux1);
-    PCReg #(.width(32)) PC (pcout, mux1, clock, start);
-    imem #(.SIZE(1024), .IMEMFILE("instr.hex")) INSTR_MEM (pcout, test);
-    extender EXT (test[15:0], extend);
+    mux_2to1_n #(.n(30)) mux0_map (adder0, adder1, mux0control, mux0);
+    mux_2to1_n #(.n(30)) mux1_map (mux0, jumpmux, jump, mux1);
+    PCReg #(.width(30)) PC (pcout, mux1, clock, start);
+    imem #(.SIZE(1024), .IMEMFILE("instr.hex")) INSTR_MEM (address, test);
+    extender #(.inN(16), .outN(30) ) EXT (test[15:0], 1'b1, extend);
     
-
-    assign jumpmux[31:26] = pcout[31:26];
+    assign address[31:2] = pcout;
+    assign address[1:0] = 2'b00;
+    assign jumpmux[29:26] = pcout[29:26];
     assign jumpmux[25:0] = test[25:0];
     
     assign instruction = test;
 
 
     initial begin
-	four = 32'h00000004;
+	one = 32'b000000000000000000000000000001;
     end
 
 endmodule
