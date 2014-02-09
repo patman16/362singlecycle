9eab2d78f15ab55af1c0fa6d968a463a06e924bc
Cristian David Bedoya <cdb539@hush.ece.northwestern.edu>
2014-02-09 14:02:33 -0600

    updated ifu files

--- ifu.v
+++ ifu.v
@@ -81,26 +81,4 @@ module ifu(branch, zero, jump, clock, start, instruction);
 
 endmodule
 
-module IFU_test();
-    reg CLK, START, BRANCH, ZERO, JUMP;
-    wire[31:0] INSTRUCTION;
-  
-    ifu IFU (.branch(BRANCH), .zero(ZERO), .jump(JUMP), .clock(CLK), .start(START), .instruction(INSTRUCTION) );
-
-  initial begin
-    BRANCH = 1'b0;
-    ZERO = 1'b0;
-    JUMP = 1'b0;
-
-    $monitor("CLK = %d INSTRUCTION =%x", CLK, INSTRUCTION);
-    #0 CLK = 1'b1;
-    #1 CLK = 1'b0;
-    #1 CLK = 1'b1;
-    #1 CLK = 1'b0;
-  end
-    
-
-
-endmodule
-
 
